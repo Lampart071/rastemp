e11e1d1c2460a705c28de9ff60a5937e8f13a95d
Lampart071 <lampart071@gmail.com>
2020-03-30 23:19:27 +0200

    Parser

--- readtemp.py
+++ readtemp.py
@@ -16,8 +16,21 @@ def read_temp_raw(folder):
     return lines
 
 
+def read_temp(folder):
+    lines = read_temp_raw(folder)
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos + 2:]
+        temp_c = float(temp_string) / 1000.0
+        #        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c
+
+
 while True:
     for index, device_file in enumerate(device_files):
-        print('SENSOR: ' + str(index) + '  ' + str(read_temp_raw(device_file)))
-    time.sleep(4)
+        print('SENSOR: ' + str(index) + '  ' + str(read_temp(device_file)))
     print('\n')
+    time.sleep(1)
